cbec61d7d814f3757fa29e33064b2b251f50efc6
esra <esrabaskaya84@gmail.com>
2024-07-02 14:43:44 +0200

    "Added DepartmentSchema and mongoose model to department.model.js"

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -1,6 +1,23 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+
+const DepartmentSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+		},
+	},
+	{
+		timestamps: true,
+		collection: "departments",
+	}
+);
+
+module.exports = mongoose.model("Department", DepartmentSchema);
