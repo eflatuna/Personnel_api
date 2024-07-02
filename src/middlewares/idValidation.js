6846f371fbed28d88a8eade52329346115d1e31f
esra <esrabaskaya84@gmail.com>
2024-07-02 16:34:47 +0200

    "Fixed syntax errors in idValidation middleware and added it to department router"

--- src/middlewares/idValidation.js
+++ src/middlewares/idValidation.js
@@ -1,9 +1,9 @@
-"use strict"
+"use strict";
 
-const {mongoose} = require("../configs/dbConnection")
+const { mongoose } = require("../configs/dbConnection");
 
-module.exports = (req,res,next) => {
-    const idIsValid = mongoose.Types.ObjectId.isValid(req.params.id); //* built-in method
-    if(!idIsValid) throw new Error("Id is not valid!")
-    next()
-}
+module.exports = (req, res, next) => {
+	const idIsValid = mongoose.Types.ObjectId.isValid(req.params.id);
+	if (!idIsValid) throw new Error("Id is not valid!");
+	next();
+};
--- src/routes/department.router.js
+++ src/routes/department.router.js
@@ -5,11 +5,13 @@
 const router = require("express").Router();
 /* ------------------------------------------------------- */
 const department = require("../controllers/department.controller");
+const idValidation = require("../middlewares/idValidation");
 //!url : /departments
 
 router.route("/").get(department.list).post(department.create);
 router
 	.route("/:id")
+	.all(idValidation)
 	.get(department.read)
 	.put(department.update)
 	.delete(department.delete);
