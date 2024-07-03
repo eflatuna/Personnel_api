7e04f256009ae9cb16cab697fd37d8c2cf8dca8c
esra <esrabaskaya84@gmail.com>
2024-07-03 22:30:25 +0200

    "Removed console.log statements for req.headers and tokenKey, and commented out console.log statements for tokenData."

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -3,17 +3,17 @@
 const Token = require("../models/token.model");
 
 module.exports = async (req, res, next) => {
-	console.log(req.headers);
+	// console.log(req.headers);
 	const auth = req.headers?.authorization || null;
 	console.log(auth);
 	const tokenKey = auth ? auth.split(" ") : null;
-	console.log(tokenKey);
+	// console.log(tokenKey);
 	if (tokenKey && tokenKey[0] == "Token") {
 		const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
 			"userId"
 		);
 
-		console.log(tokenData);
+		// console.log(tokenData);
 		if (tokenData) req.user = tokenData.userId;
 	}
 
