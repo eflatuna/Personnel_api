766267c6f519d097926580e9a82a3219da6fd661
esra <esrabaskaya84@gmail.com>
2024-07-03 10:59:43 +0200

    "Commented out department and personnel routers, added new route import, and removed index from route import"

--- index.js
+++ index.js
@@ -41,8 +41,12 @@ app.all("/", (req, res) => {
 	res.send("Welcome to the Personnel API");
 });
 
-app.use("/departments", require("./src/routes/department.router"));
-app.use("/personnels", require("./src/routes/personnel.router"));
+// app.use("/departments", require("./src/routes/department.router"));
+// app.use("/personnels", require("./src/routes/personnel.router"));
+// app.use("/tokens", require("./src/routes/token.router"));
+
+// app.use(require("./src/routes/index"));
+app.use(require("./src/routes/"));
 
 // errorHandler:
 app.use(require("./src/middlewares/errorHandler"));
