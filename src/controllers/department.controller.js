781fa1bf411373db084bfff60a5158b20141fa54
esra <esrabaskaya84@gmail.com>
2024-07-04 11:10:50 +0200

    "Added Swagger annotations to department controller and updated swagger.json file"

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -8,6 +8,20 @@ const Department = require("../models/department.model");
 module.exports = {
 	list: async (req, res) => {
 		// const departments = await res.getModelList(Department)
+
+		/*
+      #swagger.tags = ["Departments"]
+      #swagger.summary = "List Departments"
+      #swagger.description =  `
+            You can send query with endpoint for filter[], search[], sort[], page and limit.
+            <ul> Examples:
+                <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                <li>URL/?<b>page=2&limit=1</b></li>
+            </ul>
+        `
+    */
 		const data = await res.getModelList(Department);
 		res.status(200).send({
 			error: false,
